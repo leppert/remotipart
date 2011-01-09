fcbfaa8e286cc6930427a912d406e881b72814d5
Todd Eichel <todd@toddeichel.com>
2011-01-09 02:09:00 -0500

    Allow passing of options through remotipart_response to text_area_tag.

--- lib/remotipart.rb
+++ lib/remotipart.rb
@@ -1,8 +1,8 @@
 module Remotipart
-  def remotipart_response(&block)
+  def remotipart_response(options = {}, &block)
     response.content_type = Mime::HTML
     content = with_output_buffer(&block)
-    text_area_tag('remotipart_response', String.new(content))
+    text_area_tag('remotipart_response', String.new(content), options)
   end
 end
 
