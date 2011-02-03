f34fffd463e5daccb078cd618ddf99df7272c81b
Steve Schwartz <steve@alfajango.com>
2011-02-02 20:20:58 -0500

    Fixed bug with non-remotipart-submitted js response executing twice in browser.

--- lib/remotipart.rb
+++ lib/remotipart.rb
@@ -1,9 +1,12 @@
 module Remotipart
   def remotipart_response(options = {}, &block)
-    return yield unless params[:remotipart_submitted]
-    response.content_type = Mime::HTML
     content = with_output_buffer(&block)
-    text_area_tag('remotipart_response', String.new(content), options)
+    if params[:remotipart_submitted]
+      response.content_type = Mime::HTML
+      text_area_tag('remotipart_response', String.new(content), options)
+    else
+      content
+    end
   end
 end
 
