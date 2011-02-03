24421b9657a2763b90ebcc902b4a37eaf7038b4f
Steve Schwartz <steve@alfajango.com>
2011-02-02 19:57:13 -0500

    Made remotipart gem/plugin capable of handling all data-type ajax submissions, not just 'script'. Made a little more flexible, so that separate js.erb response templates are no longer needed between remotipart (i.e. multipart) ajax form submissions and regular ajax form submissions, so multiple versions of the same form can now submit to the same action.

--- lib/remotipart.rb
+++ lib/remotipart.rb
@@ -1,5 +1,6 @@
 module Remotipart
   def remotipart_response(options = {}, &block)
+    return yield unless params[:remotipart_submitted]
     response.content_type = Mime::HTML
     content = with_output_buffer(&block)
     text_area_tag('remotipart_response', String.new(content), options)
